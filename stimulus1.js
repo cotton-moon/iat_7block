b175a27e0dd6a0da0f21ac2efc73341a062f8a8c
cotton-moon <wooyueting@gmail.com>
2026-05-28 17:11:35 +0100

    Update stimulus1.js

--- stimulus1.js
+++ stimulus1.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cotton-moon.github.io/iat_7block/structure1.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({

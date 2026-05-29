476829946feaf3fabc95b9b0f4485997b15a2363
cotton-moon <wooyueting@gmail.com>
2026-05-29 15:32:07 +0100

    Update and rename stimulus3.js to stimulus4.js

--- stimulus4.js
+++ stimulus4.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cotton-moon.github.io/iat_7block/structure1.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cotton-moon.github.io/iat_7block/structure2.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
